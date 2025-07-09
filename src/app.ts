ddad0459075cf89423cf156ff51f1dd39e4eec10
zerok06 <jose.geeksjose@gmail.com>
2025-07-09 10:20:37 -0500

    feat: add type definitions for cors and morgan, enhance morgan logging format

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,10 @@ const port = process.env.PORT || 3000
 
 
 // config
-app.use(morgan('dev'))
+morgan.token('iso-date', () => new Date().toISOString());
+app.use(
+    morgan(':remote-addr [:iso-date] ":method :url HTTP/:http-version" :status :res[content-length] - :response-time ms ":referrer" ":user-agent"')
+);
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))

405abd62daef2970b65bd42645998b6af6ff6127
zerok06 <jose.geeksjose@gmail.com>
2025-08-08 11:59:41 -0500

    feat: Agregar libreria para variables de entorno.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
+import dotenv from 'dotenv'
 
+dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 3000
